0b8f123f9747cda419f9cec306a5bb68310ba5af
= <=>
2021-08-25 10:35:49 -0700

    🚀

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,42 +4,9 @@ import { Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Home() {
 
-  const [showAdminBoard, setShowAdminBoard] = useState(false);
-
-  const { user: currentUser } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    history.listen((location) => {
-      dispatch(clearMessage()); // clear message when changing location
-    });
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (currentUser) {
-      setShowAdminBoard(currentUser.roles.includes("ROLE_ADMIN"));
-    }
-  }, [currentUser]);
-
-  const logOut = () => {
-    dispatch(logout());
-  };
-
   return (
-    <Router history={history}>
-      <div>
-        <div>
-          <Switch>
-            <Route exact path={["/", "/home"]} component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/profile" component={Profile} />
-            <Route path="/user" component={BoardUser} />
-            <Route path="/admin" component={BoardAdmin} />
-            <button onClick={logOut}/>
-          </Switch>
-        </div>
-      </div>
-    </Router>
+    <div>
+      hi
+    </div>
   )
 }
